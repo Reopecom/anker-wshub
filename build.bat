96f1ed058c9c5efcd7c14fcfac78dd22dca85a4a
Ahmad Saleem Z <azsaleem11@gmail.com>
2021-10-18 09:56:53 +0200

    added exe manifest, icon

--- build.bat
+++ build.bat
@@ -1,1 +1,3 @@
- go build -o bin/
+ windres -o main-res.syso main.rc
+ go build -o bin/wshub.exe
+ "C:\Tools\signtool.exe" sign /f "E:\Github\ReopeCertificate.cer" /tr http://timestamp.comodoca.com /td sha256 bin/wshub.exe
